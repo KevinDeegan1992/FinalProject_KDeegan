00a3d21898f71c072bcea6937e5c86cbd7bca48a
Kevin Deegan <kevindeegan19@gmail.com>
2021-05-18 12:58:38 +0100

    2nd commit for Housing project

--- KevindDeegan_FinalProject/venv/HousingReport_KevinDeegan.py
+++ KevindDeegan_FinalProject/venv/HousingReport_KevinDeegan.py
@@ -8,21 +8,32 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.stats import zscore
 
-df = pd.read_csv('House_Prices.csv')
-print(df.head())
-print(df.tail())
+#df = pd.read_csv('House_Prices.csv')
+#print(df['Price'].value_counts())
+
+#group_county = df.groupby('County')
+#print(group_county.first())
+
+#print(group_county.get_group('Dublin'))
+
+#group_county_price = df.groupby(['County','Price'])
+#print(group_county_price.first())
+#print(group_county_price.last())
+
+#Need to group by county then parse into the x plot varible
+#Need to sort from low to high also
+
+#sorted_values = df.sort_values(by="Price",ascending=False)
+
+housing_subset = ["Date",
+                  "Price",
+                  "Address",
+                  "County",
+                  ]
+
+df = pd.read_csv('House_Prices.csv', usecols=housing_subset, nrows=57747)
 print(df.columns)
-print(df.shape)
-print(df.info())
 
-print(df['Price'].value_counts())
+sorting_price = df.sort_values(by="Price", ascending=True)
 
-group_county = df.groupby('County')
-print(group_county.first())
-
-print(group_county.get_group('Dublin'))
-
-group_county_price = df.groupby(['County','Price'])
-print(group_county_price.first())
-print(group_county_price.last())
 
