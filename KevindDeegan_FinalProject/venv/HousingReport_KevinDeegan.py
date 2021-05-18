33c9d7d6df46d0132c060143857cec62c1214c7d
Kevin Deegan <kevindeegan19@gmail.com>
2021-05-18 15:32:40 +0100

    4th commit for Housing project

--- KevindDeegan_FinalProject/venv/HousingReport_KevinDeegan.py
+++ KevindDeegan_FinalProject/venv/HousingReport_KevinDeegan.py
@@ -22,14 +22,21 @@ print(group_county_price.last())
 
 #Need to group by county, parse into the x plot varible
 #Need to sort from low to high also
-#For the purposes of this analysis, I will use top 5 counties: Dublin, Cork, Galway, Kildare, Meath
+#For the purposes of this analysis, I will use top 5 counties: Dublin, Cork, Galway, Kildare, Meath. For ease the cutoff point is 18000 housing units
+#Need to convert Price to float
 
 sorted_values = df.sort_values(by='Price',ascending=False)
+cutoff_point = str(500000000)
 
-top_counties = ['Date','Price','County']#list without addresses
+top_counties = ['Date','County','Price']#list without addresses
 top_county_list = pd.read_csv('House_Prices.csv', names=top_counties)
 
-print(top_county_list)
+
+
+
+
+
+
 
 
 
